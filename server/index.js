363fa91f33076251e0d6f0ab04a256aae1629046
Biki Tirua <bikitirua321@gmail.com>
2024-04-21 15:53:18 +0530

    version-v0.0.10

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routes/post.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,12 +17,12 @@ app.use(cors());
 // use routes
 app.use("/posts", router);
 
-const URL = "mongodb+srv://biki369:db321@cluster0.j46fh7r.mongodb.net/memoryDb?retryWrites=true&w=majority&appName=Cluster0";
-// const URL = "mongodb+srv://biki369:db321@cluster0.j46fh7r.mongodb.net/ ";
+// connect to db
 const PORT = process.env.PORT || 5000;
+const URL =  process.env.MONGODB_CONNECTED_URL
 
 mongoose
-  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect( URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Success");
   })
